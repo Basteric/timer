ab785d4388913804cf207b2ba6609e5e89b6be73
Bastien Manent <56586222+Basteric@users.noreply.github.com>
2024-07-06 21:36:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-const alertSound = document.getElementById('alertSound');
-
-function testSound() {
-    alertSound.play().then(() => {
-        console.log("Sound is playing");
-    }).catch((error) => {
-        console.log("Error playing sound: ", error);
-    });
-}
+let alertSound = new Audio('alert.mp3');
 
 function requestNotificationPermission() {
     if ('Notification' in window) {
@@ -22,7 +14,7 @@ function requestNotificationPermission() {
 function showNotification(taskName) {
     if ('Notification' in window && Notification.permission === 'granted') {
         new Notification('Time Tracker', {
-            body: `Task "${taskName}" has exceeded its time limit!`,
+            body: `La tâche ${taskName} a dépassé la limite de temps !`,
             icon: 'icon.png' // Add an appropriate icon if available
         });
     }
@@ -41,7 +33,9 @@ function addTask() {
         duration: 0,
         running: false,
         interval: null,
-        alerted: false
+        alerted: false,
+        state: 'stopped', // Added to track state
+        finished: false  // Added to track finished state
     };
 
     tasks.push(task);
@@ -49,6 +43,14 @@ function addTask() {
     document.getElementById('taskLimit').value = '';
     saveTasks();
     renderTasks();
+}
+
+function toggleTimer(index) {
+    if (tasks[index].running) {
+        pauseTimer(index);
+    } else {
+        startTimer(index);
+    }
 }
 
 function startTimer(index) {
@@ -61,11 +63,14 @@ function startTimer(index) {
         if (tasks[index].duration > tasks[index].limit && !tasks[index].alerted) {
             console.log(`Task "${tasks[index].name}" has exceeded its time limit!`);
             tasks[index].alerted = true;  // Set alerted to true immediately to avoid multiple triggers
-            playSoundAndNotify(tasks[index].name); // Play sound and show notification for the exceeded task
+            playSoundAndNotify(tasks[index]); // Play sound and show notification for the exceeded task
         }
         saveTasks();
         renderTasks();
     }, 1000);
+
+    tasks[index].state = 'running'; // Update state
+    updateButtonStyles(index);
 }
 
 function pauseTimer(index) {
@@ -76,15 +81,16 @@ function pauseTimer(index) {
     tasks[index].duration = Date.now() - tasks[index].startTime;
     saveTasks();
     renderTasks();
+
+    tasks[index].state = 'paused'; // Update state
+    updateButtonStyles(index);
 }
 
-function stopTimer(index) {
-    if (!tasks[index].running) return;
-
+function finishTask(index) {
     clearInterval(tasks[index].interval);
-    tasks[index].endTime = Date.now();
     tasks[index].running = false;
-    tasks[index].duration = tasks[index].endTime - tasks[index].startTime;
+    tasks[index].finished = true;
+    tasks[index].state = 'finished'; // Update state
     saveTasks();
     renderTasks();
 }
@@ -113,6 +119,10 @@ function renderTasks() {
         const taskElement = document.createElement('div');
         taskElement.className = 'task';
 
+        if (task.finished) {
+            taskElement.classList.add('finished');
+        }
+
         const hours = Math.floor(task.duration / 3600000);
         const minutes = Math.floor((task.duration % 3600000) / 60000);
         const seconds = Math.floor((task.duration % 60000) / 1000);
@@ -121,39 +131,88 @@ function renderTasks() {
 
         const durationClass = task.duration > task.limit ? 'overdue' : '';
 
+        let exceededTime = '';
+        if (task.duration > task.limit) {
+            const exceededDuration = task.duration - task.limit;
+            const exceededHours = Math.floor(exceededDuration / 3600000);
+            const exceededMinutes = Math.floor((exceededDuration % 3600000) / 60000);
+            const exceededSeconds = Math.floor((exceededDuration % 60000) / 1000);
+            exceededTime = `Dépassé de : ${exceededHours}h ${exceededMinutes}m ${exceededSeconds}s`;
+        }
+
+        const buttonLabel = task.running ? 'Pause' : 'Commencer';
+
         taskElement.innerHTML = `
+            <button class="delete-button" onclick="deleteTask(${index})">Supprimer</button>
             <h3>${task.name}</h3>
-            <p class="${durationClass}">Duration: ${hours}h ${minutes}m ${seconds}s</p>
-            <p>Limit: ${limitMinutes} minutes</p>
-            <button onclick="startTimer(${index})">Start</button>
-            <button onclick="pauseTimer(${index})">Pause</button>
-            <button onclick="stopTimer(${index})">Stop</button>
-            <button onclick="deleteTask(${index})">Delete</button>
+            <p class="${durationClass}"> <b>Durée :</b> ${hours}h ${minutes}m ${seconds}s</p>
+            <p><b>Limite</b> : ${limitMinutes} minutes</p>
+            <p>${exceededTime}</p>
+            <button id="toggle-${index}" onclick="toggleTimer(${index})">${buttonLabel}</button>
+            <button onclick="finishTask(${index})">Terminer</button>
         `;
 
         tasksContainer.appendChild(taskElement);
 
+        // Update button styles based on state
+        updateButtonStyles(index);
+
         // Check if task already exceeded its limit and alert
         if (task.duration > task.limit && !task.alerted) {
             console.log(`Task "${task.name}" has already exceeded its time limit!`);
-            playSoundAndNotify(task.name); // Play sound and show notification for the already exceeded task
+            playSoundAndNotify(task); // Play sound and show notification for the already exceeded task
             task.alerted = true;  // Set alerted to true after sound starts playing
         }
     });
 }
 
-function playSoundAndNotify(taskName) {
-    alertSound.play().then(() => {
-        console.log("Sound is playing");
-        showNotification(taskName);
-        setTimeout(() => {
-            alert("Task has exceeded its time limit! Click OK to stop the alert sound.");
-            stopSound();
-        }, 1000);  // Delay alert slightly to ensure notification and sound happen first
-    }).catch((error) => {
-        console.log("Error playing sound: ", error);
-    });
+function updateButtonStyles(index) {
+    const toggleButton = document.getElementById(`toggle-${index}`);
+    
+    if (toggleButton) {
+        toggleButton.classList.remove('button-active', 'button-paused', 'button-stopped');
+
+        if (tasks[index].state === 'running') {
+            toggleButton.classList.add('button-active');
+        } else if (tasks[index].state === 'paused') {
+            toggleButton.classList.add('button-paused');
+        } else if (tasks[index].state === 'finished') {
+            toggleButton.classList.add('button-stopped');
+            toggleButton.disabled = true;
+        }
+    }
 }
+
+function playSoundAndNotify(task) {
+    alertSound.play();
+    showNotification(task.name);
+    const exceededTime = task.duration - task.limit;
+    const exceededHours = Math.floor(exceededTime / 3600000);
+    const exceededMinutes = Math.floor((exceededTime % 3600000) / 60000);
+    const exceededSeconds = Math.floor((exceededTime % 60000) / 1000);
+    setTimeout(() => {
+        if (!document.hidden) {
+            alert(`La tâche à dépassé le temps de ${exceededHours}h ${exceededMinutes}m ${exceededSeconds}s !`);
+            stopSound();
+        }
+    }, 1000);  // Delay alert slightly to ensure notification and sound happen first
+}
+
+// Event listener to handle visibility change
+document.addEventListener('visibilitychange', () => {
+    if (!document.hidden) {
+        tasks.forEach((task, index) => {
+            if (task.alerted) {
+                stopSound();
+                const exceededTime = task.duration - task.limit;
+                const exceededHours = Math.floor(exceededTime / 3600000);
+                const exceededMinutes = Math.floor((exceededTime % 3600000) / 60000);
+                const exceededSeconds = Math.floor((exceededTime % 60000) / 1000);
+                alert(`La tâche "${task.name}" a dépassé le temps de ${exceededHours}h ${exceededMinutes}m ${exceededSeconds}s !`);
+            }
+        });
+    }
+});
 
 requestNotificationPermission();
 renderTasks();
